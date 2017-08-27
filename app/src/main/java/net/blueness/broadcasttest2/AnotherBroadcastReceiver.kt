59c9e77aa1c2c64a637d10862ac3d85ef2b1b1c0
Blueness <w0330t@gmail.com>
2017-08-27 15:58:40 +0800

    Initial commit

---
+++ app/src/main/java/net/blueness/broadcasttest2/AnotherBroadcastReceiver.kt
@@ -0,0 +1,13 @@
+package net.blueness.broadcasttest2
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class AnotherBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        Toast.makeText(context, "received in AnotherBroadcastReceiver", Toast.LENGTH_SHORT).show()
+    }
+}
